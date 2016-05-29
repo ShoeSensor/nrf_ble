c95fda500c2ab3cc45d94d2bb0cbf48feceb8ee7
Bart Monhemius <bart4524@gmail.com>
2016-05-29 14:06:05 +0200

    ble[WIP]: add place holder for the shoesensor service

---
+++ include/ble_run_service.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Bart Monhemius.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef BLE_RUN_SERVICE_H
+#define BLE_RUN_SERVICE_H
+
+#define RUN_UUID_BASE  {0X97, 0X4B, 0XCC, 0X12, 0X0A, 0XE3, 0X4F, 0XEC, 0XBE,\
+                            0XA3, 0X4A, 0XD3, 0X39, 0XD6, 0X74, 0XD0}
+
+#define RUN_UUID_PERCENTAGE 0X8E5E
+#define RUN_UUID_SPEED      0X8E5F
+
+#endif /* BLE_RUN_SERVICE_H */
